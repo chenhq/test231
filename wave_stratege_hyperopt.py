f1d047599323b9e48b28f3e4cf1b2fa500a31dde
chenhq <hongqing.chen@gmail.com>
2017-12-20 12:45:53 +0800

    update

---
+++ wave_stratege_hyperopt.py
@@ -0,0 +1,147 @@
+import multiprocessing
+import os
+import uuid
+
+import empyrical
+import pandas as pd
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials, space_eval
+
+from data_prepare import split_data_set_by_date
+from index_components import zz500
+from wave_strategy import *
+
+try:
+    import _pickle as pickle
+except:
+    import pickle
+from functools import partial
+
+absolute_spaces = {
+    'max_return_threshold':
+        hp.quniform('max_return_threshold', 0.02, 0.2, 0.01),
+    'return_per_count_threshold':
+        hp.quniform('return_per_count_threshold', 0.001, 0.02, 0.001),
+    'withdraw_threshold':
+        hp.quniform('withdraw_threshold', 0.005, 0.1, 0.005),
+    'minimum_period':
+        hp.uniform('minimum_period', 5, 20)
+}
+
+relative_spaces = {
+    'std_window':
+        hp.quniform('window', 3, 30, 2),
+    'first_count_threshold':
+        hp.quniform('first_count_threshold', 0.1, 3.0, 0.1),
+    'return_per_count_threshold':
+        hp.quniform('return_per_count_threshold', 0.1, 3.0, 0.1),
+    'withdraw_threshold':
+        hp.quniform('withdraw_threshold', 0.2, 4, 0.2)
+}
+
+
+def get_data(stk_list):
+    # market = pd.read_csv("../data/cs_market.csv", parse_dates=["date"], dtype={"code": str})
+    # market = pd.read_csv("~/cs_market.csv", parse_dates=["date"], dtype={"code": str})
+    market = pd.read_csv("E:\market_data/cs_market.csv", parse_dates=["date"], dtype={"code": str})
+    all_ohlcv = market.drop(["Unnamed: 0", "total_turnover", "limit_up", "limit_down"], axis=1)
+    all_ohlcv = all_ohlcv.set_index(['code', 'date']).sort_index()
+    idx_slice = pd.IndexSlice
+    stk_ohlcv_list = []
+    for stk in all_ohlcv.index.get_level_values('code').unique():
+        if stk in stk_list:
+            stk_ohlcv = all_ohlcv.loc[idx_slice[stk, :], idx_slice[:]]
+            stk_ohlcv_list.append(stk_ohlcv)
+    return stk_ohlcv_list
+
+
+def valid_wave_by_multi_processes(params, ohlcv_list, operation, mode, processes=0):
+    stk_result_list = []
+
+    if processes <= 0:
+        processes = multiprocessing.cpu_count()
+
+    pool = multiprocessing.Pool(processes=processes)
+
+    def callback(result):
+        stk_result_list.append(result)
+
+    def print_error(err):
+        print(err)
+
+    for ohlcv in ohlcv_list:
+        if 'std_window' in params:
+            std_window = params['std_window']
+        else:
+            std_window = 0
+        pool.apply_async(tag_wave_direction, args=(
+            ohlcv, params['first_count_threshold'], params['return_per_count_threshold'],
+            params['withdraw_threshold'], operation, mode, std_window,),
+                         callback=callback, error_callback=print_error)
+
+    pool.close()
+    pool.join()
+    return stk_result_list
+
+
+def objective(params, ohlcv_list, operation, mode, log_dir):
+    print(params)
+    identity = str(uuid.uuid1())
+    result_list = valid_wave_by_multi_processes(params, ohlcv_list, operation, mode)
+    returns_list = []
+    for result in result_list:
+        stk_returns = result['pct_chg'] * result['direction']
+        stk_returns = stk_returns.fillna(0)
+        returns_list.append(stk_returns)
+    returns = pd.concat(returns_list, axis=0)
+
+    annual_return = empyrical.annual_return(returns)
+    sharpe_ratio = empyrical.sharpe_ratio(returns)
+    if np.isnan(sharpe_ratio):
+        sharpe_ratio = 0
+
+    data = {
+        'id': identity,
+        'params': params,
+        'returns': returns,
+        'annual_return': annual_return,
+        'sharpe_ratio': sharpe_ratio,
+        # 'result_list': result_list
+    }
+
+    with open(os.path.join(log_dir, identity + '.pkl'), 'wb') as f:
+        pickle.dump(data, f)
+
+    print('id: %s, annual_return: %s, sharpe_ratio: %s' % (identity, annual_return, sharpe_ratio))
+    return {'loss': -sharpe_ratio, 'status': STATUS_OK}
+
+
+if __name__ == '__main__':
+    space = relative_spaces
+    sub_dir = 'relative'
+
+    # function = tag_wave_direction_by_absolute()
+    # space = absolute_spaces
+    # sub_dir = 'absolute'
+
+    ohlcv_list = get_data(zz500[:50])
+    split_dates = ["2016-01-01", "2017-01-01"]
+    train_set, validate_set, test_set = split_data_set_by_date(ohlcv_list, split_dates, minimum_size=1)
+    log_dir = os.path.join('./valid_wave_hyperopt', sub_dir)
+
+    if not os.path.isdir(log_dir):
+        os.makedirs(log_dir)
+
+    hyperopt_objective = partial(objective, ohlcv_list=test_set, operation='search', mode='relative', log_dir=log_dir)
+    trials = Trials()
+    best = fmin(hyperopt_objective, space, algo=tpe.suggest, max_evals=60, trials=trials)
+    params = space_eval(space, best)
+    print(params)
+
+    # best_params = {
+    #     'minimum_period': 12.135881002390583,
+    #     'std_window': 10.0,
+    #     'withdraw_threshold': 2.0,
+    #     'max_return_threshold': 1.0,
+    #     'return_per_count_threshold': 1.0}
+    #
+    # hyperopt_objective(best_params)
---
+++ wave_strategy.py
@@ -0,0 +1,118 @@
+import numpy as np
+
+
+# operation='search'/'label'
+# mode='relative'/'absolute'
+def tag_wave_direction(ohlcv, first_count_threshold, return_per_count_threshold,
+                       withdraw_threshold, operation='search', mode='relative', std_window=30):
+    ohlcv['pct_chg'] = (ohlcv['close'] / ohlcv['close'].shift(1) - 1).fillna(0)
+    ohlcv['std'] = ohlcv['pct_chg'].rolling(int(std_window)).std().bfill(0)
+    ohlcv['direction'] = np.nan
+    i = 0
+    while i < len(ohlcv):
+        # 波段的周期数量
+        count = 1
+        price = ohlcv.iloc[i]['open']
+        if ohlcv.iloc[i]['close'] > ohlcv.iloc[i]['open']:
+            min_price = ohlcv.iloc[i]['open']
+            max_price = ohlcv.iloc[i]['close']
+            direction = 1
+            returns = (ohlcv.iloc[i]['close'] / ohlcv.iloc[i]['open']) - 1
+            if returns < first_count_threshold * ohlcv.iloc[i]['std']:
+                i += 1
+                continue
+        elif ohlcv.iloc[i]['close'] < ohlcv.iloc[i]['open']:
+            min_price = ohlcv.iloc[i]['close']
+            max_price = ohlcv.iloc[i]['open']
+            direction = -1
+            returns = 1 - (ohlcv.iloc[i]['close'] / ohlcv.iloc[i]['open'])
+            if returns < first_count_threshold * ohlcv.iloc[i]['std']:
+                i += 1
+                continue
+        else:
+            i += 1
+            continue
+
+        argmin = i
+        argmax = i
+        # print(ohlcv.index[i], direction, price)
+        j = i + 1
+        if j >= len(ohlcv):
+            i += 1
+            continue
+        while j < len(ohlcv):
+            count += 1
+            new_price = ohlcv.iloc[j]['close']
+
+            if new_price > max_price:
+                max_price = new_price
+                argmax = j
+
+            if new_price < min_price:
+                min_price = new_price
+                argmin = j
+
+            returns = (new_price / price - 1) * direction
+            return_per_count = returns / count
+
+            if direction == 1:
+                withdraw = (max_price - new_price) / price
+                max_return_pos = argmax
+            elif direction == -1:
+                withdraw = (new_price - min_price) / price
+                max_return_pos = argmin
+
+            if mode == 'relative':
+                return_per_count_threshold_ = return_per_count_threshold * ohlcv.iloc[j]['std']
+                withdraw_threshold_ = withdraw_threshold * ohlcv.iloc[j]['std']
+            else:
+                return_per_count_threshold_ = return_per_count_threshold
+                withdraw_threshold_ = withdraw_threshold
+
+            # 波段趋势太小，未继续满足return_per_count_threshold_而结束
+            if return_per_count < return_per_count_threshold_:
+                wave_end_pos = i
+                # 波段满足max_return_threshold or minimum period
+                # if max_return > max_return_threshold_ or max_return_pos - i - 1 >= minimum_period:
+                # 确定最后的标签位置
+                if operation == 'search':
+                    wave_end_pos = j
+                elif operation == 'label':
+                    wave_end_pos = max_return_pos
+
+                # 符合条件的波段
+                # 标记[i, wave_end_pos]的direction标签
+                for k in range(i + 1, wave_end_pos + 1):
+                    ohlcv.loc[ohlcv.index[k], 'direction'] = direction
+                i = wave_end_pos + 1
+                break
+
+            # 波段因为最大回撤结束
+            if withdraw > withdraw_threshold_:
+                wave_end_pos = i
+                # 满足max_return_threshold or minimum period
+                # if max_return > max_return_threshold_ or max_return_pos - i - 1 >= minimum_period:
+                # 确定最后的标签位置
+                if operation == 'search':
+                    wave_end_pos = j
+                elif operation == 'label':
+                    wave_end_pos = max_return_pos
+
+                # 符合条件的波段
+                # 标记[i, wave_end_pos]的direction标签
+                for k in range(i + 1, wave_end_pos + 1):
+                    ohlcv.loc[ohlcv.index[k], 'direction'] = direction
+                i = wave_end_pos + 1
+                break
+            j += 1
+            if j >= len(ohlcv):
+                i += 1
+    # # show
+    # result = ohlcv.reset_index().reset_index()
+    # fig, ax = plt.subplots(1, figsize=(21, 7))
+    # result.plot(x='index', y='close', figsize=(21, 7), ax=ax)
+    # result[result['direction'] > 0].plot.scatter(x='index', y='close', s=10, c='r', figsize=(21, 7), ax=ax)
+    # result[result['direction'] < 0].plot.scatter(x='index', y='close', s=10, c='g', figsize=(21, 7), ax=ax)
+    # result[np.isnan(result['direction'])].plot.scatter(x='index', y='close', s=10, c='b', figsize=(21, 7), ax=ax)
+    # plt.show()
+    return ohlcv
